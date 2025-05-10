de502f10ad321276f32fad3f8040798af3e128b6
Chaim Mendes <chaim.mendes@gmail.com>
2025-05-10 18:00:08 +0530

    Landing Completed

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,42 @@
 const textToType = [
-    ">evaloping",
-    ">evalopin",
-    ">evalopi",
-    ">evalop",
-    ">evalo",
-    ">eval",
-    ">eva",
-    ">ev",
-    ">e",
+    "eveloping ",
+    "reating ",
+    "haim Mendes "
+];
+const Capitals = [
     ">",
-    "",
+    "<",
+    "<"
 ]
+// Index's
+let messageIndex = 0;
+let charIndex = 0;
+
+// Constants
+const typingInterval = 100;
+const pause = 2000;
+const element = document.getElementById("dynamic-txt");
+const capital = document.getElementById("dynamic-txt-Capital");
+
+function typeMessage(){
+    if(charIndex < textToType[messageIndex].length){
+        if (charIndex == 0){
+            capital.textContent = "/" + Capitals[messageIndex]
+        }else{
+            element.textContent += textToType[messageIndex][charIndex-1]
+        }
+        charIndex++;
+        setTimeout(typeMessage, typingInterval)
+    } else {
+        setTimeout(()=> {
+            element.textContent = "";
+            capital.textContent = "/";
+            charIndex = 0;
+            messageIndex = (messageIndex + 1)%textToType.length;
+            typeMessage();
+        }, pause);
+        
+    }
+}
+element.textContent = "";
+typeMessage()
