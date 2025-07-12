01a72993a78b674f9b19ce2ac4d78548102aa962
Chaim Mendes <chaim.mendes@gmail.com>
2025-07-12 20:29:35 +0530

    Carrousell functionality

--- Scripts/script.js
+++ Scripts/script.js
@@ -18,24 +18,24 @@ const pause = 2000;
 const element = document.getElementById("dynamic-txt");
 const capital = document.getElementById("dynamic-txt-Capital");
 
-function typeMessage(){
-    if(charIndex < textToType[messageIndex].length){
-        if (charIndex == 0){
+function typeMessage() {
+    if (charIndex < textToType[messageIndex].length) {
+        if (charIndex == 0) {
             capital.textContent = "/" + Capitals[messageIndex]
-        }else{
-            element.textContent += textToType[messageIndex][charIndex-1]
+        } else {
+            element.textContent += textToType[messageIndex][charIndex - 1]
         }
         charIndex++;
         setTimeout(typeMessage, typingInterval)
     } else {
-        setTimeout(()=> {
+        setTimeout(() => {
             element.textContent = "";
             capital.textContent = "/";
             charIndex = 0;
-            messageIndex = (messageIndex + 1)%textToType.length;
+            messageIndex = (messageIndex + 1) % textToType.length;
             typeMessage();
         }, pause);
-        
+
     }
 }
 element.textContent = "";
@@ -46,7 +46,7 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show")
-        } else{
+        } else {
             entry.target.classList.remove("show")
         }
     })
@@ -55,3 +55,36 @@ const observer = new IntersectionObserver(entries => {
 popoutFont.forEach(el =>
     observer.observe(el)
 )
+
+const items = document.querySelectorAll(".PortfolioCarrouselItem");
+const prevBtn = document.getElementsByClassName('CarrouselPrev')[0];
+const nextBtn = document.getElementsByClassName('CarrouselNext')[0];
+let currentScroll = 0;
+const itemsCount = items.length - 1;
+function scrollMyCarrousel(direction) {
+    if (currentScroll === -itemsCount && direction === -1) {
+        currentScroll = 0;
+        console.log("Resetting to 0");
+    } else if (currentScroll === 0 && direction === 1) {
+        currentScroll = -itemsCount;
+        console.log("Resetting to last item");
+    } else {
+        currentScroll += direction;
+        console.log(`Current Scroll: ${currentScroll}`);
+    }
+    items.forEach((item, index) => {
+        item.style.transform = `translateX(${currentScroll * 100}%)`;
+    }
+    );
+}
+
+// prevBtn.addEventListener('click', scrollMyCarrousel(-1));
+// nextBtn.addEventListener('click', scrollMyCarrousel(1));
+prevBtn.addEventListener('click', () => {
+    console.log("Previous button clicked");
+    scrollMyCarrousel(1);
+});
+nextBtn.addEventListener('click', () => {
+    console.log("Next button clicked");
+    scrollMyCarrousel(-1);
+});
